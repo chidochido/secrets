5d7668a16b887b74bca3bab0b25892fc0e2a63de
Chidozie Nnaduruaku <nchidozien@gmail.com>
2023-01-15 12:29:47 -0500

    Make messages expire after two minutes

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,19 +1,21 @@
 const mongoose = require('mongoose')
 
-const messageSchema = mongoose.Schema(
-	{
-		content: {
-			type: String,
-			required: true,
-		},
-
-		password: {
-			type: String,
-			required: false,
-		},
+const messageSchema = mongoose.Schema({
+	content: {
+		type: String,
+		required: true,
 	},
 
-	{ timestamps: true }
-)
+	password: {
+		type: String,
+		required: false,
+	},
+
+	expireAt: {
+		type: Date,
+		default: Date.now,
+		expires: 120,
+	},
+})
 
 module.exports = mongoose.model('Message', messageSchema)
